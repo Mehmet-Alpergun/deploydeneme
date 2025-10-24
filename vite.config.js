ea02d3b34285c1a64abfe2b09a1d6ee016facb0b
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2025-10-24 21:07:56 +0300

    sdasdasdyeterlan

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/deploydeneme/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        menu: resolve(__dirname, "menu.html"),
+        cart: resolve(__dirname, "cart.html"),
+        register: resolve(__dirname, "register.html"),
+        signin: resolve(__dirname, "signin.html"),
+      },
+    },
+  },
 });
